24124cfe63ea6477cb13ebcddfcb319dfa29d374
Ellie Keen <elliekeen95@gmail.com>
2018-07-23 10:55:29 +0100

    Add buy method

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,10 +1,16 @@
 class VendingMachine
 
-  attr_reader :crisps_stock, :water_stock
+  attr_reader :crisps_stock, :water_stock, :bought_items
 
   def initialize
     @crisps_stock = ["Crisps", "Crisps", "Crisps"]
     @water_stock = ["Water", "Water", "Water"]
+    @bought_items = []
+  end
+
+  def buy(item)
+    item == "Water" ? @water_stock.pop : @crisps_stock.pop
+    @bought_items << item
   end
 
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -12,4 +12,20 @@ describe 'Vending Machine' do
     expect(vending_machine.water_stock).to eq(["Water", "Water", "Water"])
   end
 
+  describe '#buy' do
+    it "should remove water from water_stock array" do
+      vending_machine.buy("Water")
+      expect(vending_machine.water_stock).to eq(["Water", "Water"])
+    end
+
+    it "should remove crisps from crisps_stock array" do
+      vending_machine.buy("Crisps")
+      expect(vending_machine.crisps_stock).to eq(["Crisps", "Crisps"])
+    end
+
+    it "should add item to bought_items" do
+       expect{vending_machine.buy("Water")}.to change{vending_machine.bought_items.length}.from(0).to(1)
+    end
+  end
+
 end
